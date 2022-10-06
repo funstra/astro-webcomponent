40f6969bfc5644347158a991e8a19dd9fd6f593d
funstra <founstra@gmail.com>
2022-10-06 22:45:04 +0200

    compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import compres from "astro-compress";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [compres()],
+});
