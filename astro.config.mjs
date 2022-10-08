d87876edd9b3a94205f46ab66cb2e8e4289e82f1
funstra <founstra@gmail.com>
2022-10-08 17:15:21 +0200

    mmmm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from "astro/config";
+import { chunkSplitPlugin } from "vite-plugin-chunk-split";
 import compres from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [
+      chunkSplitPlugin({
+        strategy: "unbundle",
+      }),
+    ],
+  },
   integrations: [compres()],
 });
--- src/scripts/rooter.ts
+++ src/scripts/rooter.ts
@@ -1,3 +1,4 @@
+
 const roots: { href: string, document: Document }[] = []
 
 if (!roots.find(root => root.href === location.href)) {
@@ -9,6 +10,10 @@ if (!roots.find(root => root.href === location.href)) {
 
 
 async function diff_styles(target_document: Document) {
+
+    console.log("DIFF")
+    
+    
     const target_links = [...target_document.querySelectorAll('link[rel="stylesheet"]')] as HTMLLinkElement[]
     const current_links = [...document.querySelectorAll('link[rel="stylesheet"]')] as HTMLLinkElement[]
     const filtered_links = target_links.filter(target_link => !current_links.some(current_link => target_link.href === current_link.href))
@@ -30,6 +35,23 @@ async function diff_styles(target_document: Document) {
     const filtered_styles = target_styles.filter(target_style => !current_styles.some(current_style => target_style.textContent === current_style.textContent))
 
     document.head.append(...filtered_styles)
+
+
+    const targetScripts = [...target_document.querySelectorAll('script')]
+    const currentScripts = [...document.querySelectorAll('script')]
+    const filtered_scripts = targetScripts.filter(target_script => !currentScripts.some(current_script => target_script.src === current_script.src))
+
+
+
+    // filtered_scripts.forEach(async s => {
+    //     console.log('hello')
+    //     const text = await (await fetch(s.src)).text()
+    //     const script = document.createElement('script')
+    //     script.innerHTML = text
+    //     document.head.appendChild(script)
+    // })
+    console.log(filtered_scripts)
+    document.head.append(...filtered_scripts)
 
     return true
 }
@@ -59,6 +81,7 @@ async function transition(href: string) {
 
         const target_document_string = await fetch(href).then(res => res.text())
         target_document = new DOMParser().parseFromString(target_document_string, 'text/html')
+        console.log(target_document)
         await diff_styles(target_document)
         document.dispatchEvent(new Event('rooter:fetching-done'))
         document.documentElement.classList.remove('is-fetching')
